efd25b90659d3e7611a47af13063df8210b496c0
Khanyisa <kngalwana@yahoo.com>
2019-07-31 14:03:52 +0200

    Customer variables

---
+++ Customer.java
@@ -0,0 +1,17 @@
+public class Customer{
+
+String fullNames;
+int pin;
+double balance;
+double accountBalance;
+double withdrawalAmount;
+double depositAmount;
+double minimumWithrawalAmount;
+double maximumWithdrawalAmount;
+double minimumDepositAmount;
+double maximumDepositAmount;
+double checkBalance;
+String reference;
+
+
+}
