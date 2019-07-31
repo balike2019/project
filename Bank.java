74dff158f6a864389c4d60e9ea3e046d6fe4d91c
herick achu <herickoachu@gmail.com>
2019-07-31 22:56:32 +0200

    new

---
+++ Bank.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Bank{
+String name;
+long  callCenter;
+String  physicalAddress;
+String emailAddress;
+String printReceipt;
+
+
+public void printReceipt(Customer c){
+System.out.println(""**********welcome to FNB*************");
+System.out.println("JBURG Branch");
+System.out.println("call center #0875433245");
+System.out.println("fnb.co.za");
+
+
+
+}
+}
