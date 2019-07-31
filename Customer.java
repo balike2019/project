c12c445a966df32d71e5969f50581f124484ae1c
babiaka lizette <babiakalizette@gmail.com>
2019-07-31 15:10:54 +0200

    new

---
+++ Customer.java
@@ -0,0 +1,16 @@
+class Customer{
+public String name;
+public String emailAddress;
+public String gender;
+public long phoneNumber;
+public long accountNumber;
+public  double  depositAmount;
+public double checkBalance;
+public double withdrawalAmount;
+public  double mimimumAmount;
+public double maximumAmount;
+public int pin;
+public String insertCard;
+public String accountBalance;
+
+}
