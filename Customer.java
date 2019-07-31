e17195164ca71f2adb979225ecfb33eb02c3d73d
cho hyacinth <chohyacinth@gmail.com>
2019-07-31 13:54:34 +0200

    customer variables

---
+++ Customer.java
@@ -0,0 +1,16 @@
+public class Customer{
+String fullNames;
+int pin;
+double balance;
+double amount;
+double accountBalance;
+double withdrawalamount;
+double depositAmount;
+double minimumWithdrawalAmount;
+double maximumWithdrawalAmount;
+double minimumDepositAmount;
+double maximumDepositAmount;
+double checkBalance;
+String reference;
+
+}
