0dc5ba2e9aa8c91ddaa8399ed20eaf4ba9bf7756
Khanyisa <kngalwana@yahoo.com>
2019-08-02 21:06:22 +0200

    fixed some errors

--- Customer.java
+++ Customer.java
@@ -1,54 +1,31 @@
 import java.util.Scanner;
-public class Customer{
-Scanner = sc.Scanner (System.in);
 
+public class Customer{
 public String  fullNames;
 public String emailAddress;
 public String gender;
 public long phoneNumber;
 public long accountNumber;
-public  double  depositAmount;
+public double  depositAmount;
 public double checkBalance;
 public double withdrawalAmount;
-public  double mimimumAmount;
+public double minimumAmount;
 public double maximumAmount;
 public int pin;
 public String insertCard;
 public String accountBalance;
 public String reference;
+public double balance;
+public double amount;
+public double minimumWithdrawalAmount;
+public double maximumWithdrawalAmount;
+public double minimumDepositAmount;
+public double maximumDepositAmount;
+public double checkBalance;
 
-<<<<<<< HEAD
 Scanner sc = new Scanner(System.in);
-=======
-=======
-import java.util.Scanner;
-public class Customer{
 
-String fullNames;
-int pin;
-double balance;
-double amount;
-long accountNumber;
-double accountBalance;
-double withdrawalAmount;
-double depositAmount;
-double minimumWithdrawalAmount;
->>>>>>> e17195164ca71f2adb979225ecfb33eb02c3d73d
-double maximumWithdrawalAmount;
-double minimumDepositAmount;
-double maximumDepositAmount;
-double checkBalance;
-String reference;
-<<<<<<< HEAD
-
-Scanner=sc.Scanner(System.in);
-
-<<<<<<< HEAD
 public double deposit(){
-=======
->>>>>>> 8b0a4e69753c7549b11da507a25c9cc970703da2
-void deposit(){
->>>>>>> acda2a41007b2fd2ec6b7a4172f6ecc21599d12d
 System.out.println(" insert card");
 
 System.out.println("Enter accountNumber");
@@ -64,9 +41,9 @@ System.out.println("reference= "+reference);
 sc.nextLine();
 System.out.println("Press 1 to continue or 2 to cancel");
 
-<<<<<<< HEAD
-System.out.println("Enter fullName");
-fullName = sc.nextLine();
+
+System.out.println("Enter fullNames");
+fullNames = sc.nextLine();
 
 System.out.println("Enter phoneNumber");
 phoneNumber = sc.nextLong();
@@ -90,8 +67,8 @@ System.out.println("Proceed");
 System.out.println("Cancel");
 sc.nextLine();
 
-if (accountNumber==){
-System.out.println("Successful")
+if (accountNumber==accountNumber){
+System.out.println("Successful");
 }else{
 System.out.println("Fail");
 depositAmount = balance + depositAmount;
@@ -100,11 +77,8 @@ return balance;
 
 }
 
-long   phoneNumber;
-String emailAdress;
 
-=======
-double checkBalance(){;
+double checkBalance(){
 System.out.println("Enter pin");
 pin =sc. nextInt();
 System.out.println("select balance");
@@ -112,22 +86,22 @@ System.out.println("1)print balance ,2)preview balance");
 
 int select;
 select = sc.nextInt();
-if(select == 1);
+if(select == 1){
 System.out.println("print balance");
 }else{
 System.out.println("preview");
 
 }
->>>>>>> acda2a41007b2fd2ec6b7a4172f6ecc21599d12d
-}
 
 }
 
-public double withdrawalAmunt(){
+}
+
+public double withdrawalAmount(){
 System.out.println("INSERT YOUR CARD");
 System.out.println(" Enter pin");
 pin = sc.nextInt();
-System.out.println("login successful")
+System.out.println("login successful");
 
 System.out.println("Choose Amount");
 System.out.println("1 = R100");
